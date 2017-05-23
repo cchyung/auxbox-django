269f25646cf301612e2d8a7d9a87c1b5f03bce61
Conner Chyung <chyungspice@gmail.com>
2017-05-22 19:37:02 -0600

    Update wsgi.py

--- auxbox/wsgi.py
+++ auxbox/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "auxbox.settings.base")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'aux.settings.base'
 
 application = get_wsgi_application()
