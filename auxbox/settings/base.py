c3d1ac81d1b3dccc37ba77b0218e29a58ee5d68a
Conner Chyung <chyungspice@gmail.com>
2017-05-23 22:40:29 -0600

    Update to use postgresql

--- auxbox/settings/base.py
+++ auxbox/settings/base.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'auxbox.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'auxbox_dev',
+        'USER': 'conner',
+        'PASSWORD': 'developer',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
