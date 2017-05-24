3db31acb39e5aec012d99da06b048c8b8cad0158
Conner Chyung <chyungspice@gmail.com>
2017-05-23 20:08:09 -0600

    Update settings

--- auxbox/settings/local.py
+++ auxbox/settings/local.py
@@ -1,7 +1,9 @@
-from .base import *
+from auxbox.settings.base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = [
+ALLOWED_HOSTS += [
     u'localhost',
 ]
+
+SECRET_KEY = '=w@ualtox7^w203!+^inc*f8z+#e3*2zs7%vjv4qw0rqc$yy@0'
