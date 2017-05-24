598f63f3205884ef5d47323dcd1185907e022ee6
Conner Chyung <chyungspice@gmail.com>
2017-05-23 22:45:55 -0600

    Install dj-database-url

--- auxbox/settings/base.py
+++ auxbox/settings/base.py
@@ -1,5 +1,7 @@
 import os
 
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -68,6 +70,8 @@ DATABASES = {
         'PORT': '5432'
     }
 }
+
+
 
 
 # Password validation
