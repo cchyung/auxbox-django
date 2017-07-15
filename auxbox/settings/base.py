0d538de5f6c134130fa81bada8a0a35f8fc55fc2
Conner Chyung <chyungspice@gmail.com>
2017-07-14 20:55:57 -0600

    Update settings

--- auxbox/settings/base.py
+++ auxbox/settings/base.py
@@ -70,22 +70,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'auxbox.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'auxbox_dev',
-        'USER': 'conner',
-        'PASSWORD': 'developer',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
-
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
--- auxbox/settings/local.py
+++ auxbox/settings/local.py
@@ -9,3 +9,18 @@ INSTALLED_APPS += [
 ALLOWED_HOSTS += [
     u'localhost',
 ]
+
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'auxbox_dev',
+        'USER': 'conner',
+        'PASSWORD': 'developer',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
